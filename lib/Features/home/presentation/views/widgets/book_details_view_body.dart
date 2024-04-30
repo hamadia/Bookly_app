ccb6f8481fa54ad8df92ad345ed3d1b00ab0018d
ASUS <hamadiamahmou@gamil.com>
2024-05-01 00:58:27 +0300

    + custom book item to book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,10 +8,18 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomBookDetailsAppBar(),
-      ],
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomBookItem(),
+          ),
+        ],
+      ),
     );
   }
 }
