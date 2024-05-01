f77b2adb0b919fbc8993aa3dba4bbf49f841cfd9
ASUS <hamadiamahmou@gamil.com>
2024-05-01 19:24:47 +0300

    + book model

---
+++ lib/Features/home/data/model/book_model/ListPrice.dart
@@ -0,0 +1,21 @@
+class ListPrice {
+  ListPrice({
+    this.amountInMicros,
+    this.currencyCode,
+  });
+
+  ListPrice.fromJson(dynamic json) {
+    amountInMicros = json['amountInMicros'];
+    currencyCode = json['currencyCode'];
+  }
+
+  int? amountInMicros;
+  String? currencyCode;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['amountInMicros'] = amountInMicros;
+    map['currencyCode'] = currencyCode;
+    return map;
+  }
+}
