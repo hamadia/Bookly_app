bba35e0e00647df5aabe78a6d93ef54fcab82acb
ASUS <hamadiamahmou@gamil.com>
2024-05-01 18:58:02 +0300

    + newest books use case

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -6,5 +6,5 @@ import '../entities/book_entity.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
 
-  Future<Either<Failure, List<BookEntity>>> fetchNewsedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
