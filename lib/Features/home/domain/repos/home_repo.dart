c3177350be63abc322d026fb22ebfc92ca765f11
ASUS <hamadiamahmou@gamil.com>
2024-05-01 17:59:04 +0300

    + Either class

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 import '../entities/book_entity.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
 
-  Future<List<BookEntity>> fetchNewsedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsedBooks();
 }
